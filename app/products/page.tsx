7b47267371dc9a43326cc9c46d798eba684c538a
codegreen <codegreen19s@gmail.com>
2024-09-07 15:47:23 +0600

    first commit

---
+++ app/products/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam fuga quis
+      ipsa voluptatibus! Aliquid cumque, animi repellat autem unde voluptatum
+      voluptates dolores laborum nisi. Voluptas recusandae incidunt mollitia
+      harum dolorem.
+    </div>
+  );
+};
+
+export default Products;
---
+++ app/signup/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SignUpPage = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quo
+      quia enim beatae, placeat explicabo saepe quidem architecto amet
+      necessitatibus dolorum vel, praesentium eaque tempore harum voluptates
+      recusandae earum magni.
+    </div>
+  );
+};
+
+export default SignUpPage;
